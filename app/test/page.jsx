f09f572760af4d3e8afd91ce46b4ed5a16f8c401
yosefus <yosalsoni@gmail.com>
2025-01-25 21:24:26 +0200

    first config

---
+++ app/test/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './style.module.scss'
+
+export default function Test() {
+  return (
+    <div>
+        <h1 className={styles.title}>
+            לך אל נמלה עצל
+        </h1>
+    </div>
+  )
+}
