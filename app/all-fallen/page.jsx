261c8f81d109ec250402cf40ee732d5c6c02216d
shahaf noked <shahafnoked@gmail.com>
2025-01-26 12:21:11 +0200

    Add layout components and new pages for About, AllFallen, Contact, and FallenProfile

---
+++ app/all-fallen/page.jsx
@@ -0,0 +1,6 @@
+import styles from './page.module.scss'
+export default function AllFallenPage() {
+    return (
+        <div>AllFallenPage</div>
+    )
+}
---
+++ app/components/Header/index.jsx
@@ -0,0 +1,7 @@
+import styles from './style.module.scss'
+
+export default function Header() {
+    return (
+        <header>Header</header>
+    )
+}
