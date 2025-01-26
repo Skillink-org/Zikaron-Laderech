04e9ebf583d5dc184531f9a3cd83b428ecc0e590
shahaf noked <shahafnoked@gmail.com>
2025-01-26 13:19:32 +0200

    Header component, styling and navigation links

--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -1,7 +1,17 @@
+import Link from 'next/link'
 import styles from './style.module.scss'
 
 export default function Header() {
     return (
-        <header>Header</header>
+        <header className={styles.header}>
+            <div className={styles.logo}><span>זיכרון </span>לדרך</div>
+            <nav className={styles.nav}>
+                <Link href='/'>בית</Link>
+                <Link href='/all-fallen'>השמות והתחביבים</Link>
+                <Link href='/about'>אודות</Link>
+                <Link href='/contact'>יצירת קשר</Link>
+            </nav>
+            <input type="text" />
+        </header>
     )
 }
