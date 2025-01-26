e6659f0917d0b7b4e1e699b15c3fadfcaf12cfc3
shahaf noked <shahafnoked@gmail.com>
2025-01-26 13:38:26 +0200

    Add Footer component with navigation links and styling

--- app/components/Footer/index.jsx
+++ app/components/Footer/index.jsx
@@ -1,5 +1,17 @@
+import Link from 'next/link'
+import styles from './style.module.scss'
+
 export default function Footer() {
     return (
-        <footer>Footer</footer>
+        <footer className={styles.footer}>
+            <nav className={styles.nav}>
+                <Link href='/'>בית</Link>
+                <Link href='/'>השמות והתחביבים</Link>
+                <Link href='/'>אודות</Link>
+                <Link href='/'>יצירת קשר</Link>
+                <Link href='/'>הוספת נופל</Link>
+            </nav>
+            <small className={styles.small}>2025 | נבנה ופותח ע”י Skillink</small>
+        </footer>
     )
 }
