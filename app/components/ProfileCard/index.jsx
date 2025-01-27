fdc86cd2849774a047f15c2558ff07d5a4673fff
kislevlevy <kislev.levy@gmail.com>
2025-01-27 16:56:40 +0200

    ProfileCard fix
    1. Applied prettier formats for better readability.
    2. Removed unnecessary imports.

--- app/components/ProfileCard/index.jsx
+++ app/components/ProfileCard/index.jsx
@@ -1,21 +1,21 @@
-import React from "react";
 import styles from "./style.module.scss";
 
-const ProfileCard = ({ firstName, lastName, birthYear, deathYear, imageSrc }) => {
-    return (
-        <div className={styles.card}>
-            <div className={styles.profileImage}>
-                <img
-                    src={imageSrc}
-                    alt={`${firstName} ${lastName}`}
-                />
-            </div>
-            <h3 className={styles.name}>{`${firstName} ${lastName}`}</h3>
-            <p className={styles.years}>
-                {birthYear} - {deathYear}
-            </p>
-        </div>
-    );
-};
-
-export default ProfileCard;
+export default function ProfileCard({
+  firstName,
+  lastName,
+  birthYear,
+  deathYear,
+  imageSrc,
+}) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.profileImage}>
+        <img src={imageSrc} alt={`${firstName} ${lastName}`} />
+      </div>
+      <h3 className={styles.name}>{`${firstName} ${lastName}`}</h3>
+      <p className={styles.years}>
+        {birthYear} - {deathYear}
+      </p>
+    </div>
+  );
+}
