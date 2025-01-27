9a2c08717307e7da5928c9e6d4aa9b525254bbb6
DESKTOP-5Q9K2NL\user1 <chay6865@gmail.com>
2025-01-27 09:54:13 +0200

    Change name of page to index, and move styles to scss pags.

--- app/components/ProfileCard/index.jsx
+++ app/components/ProfileCard/index.jsx
@@ -1,19 +1,16 @@
 import React from "react";
-import styles from "./profile-card.module.scss";
-import Image from 'next/image';
+import styles from "./style.module.scss";
 
 const ProfileCard = ({ firstName, lastName, birthYear, deathYear, imageSrc }) => {
     return (
         <div className={styles.card}>
             <div className={styles.profileImage}>
-                <Image
+                <img
                     src={imageSrc}
-                    alt={firstName + " " + lastName}
-                    layout="fill"
-                    objectFit="cover"
+                    alt={`${firstName} ${lastName}`}
                 />
             </div>
-            <h3 className={styles.name}>{firstName + " " + lastName}</h3>
+            <h3 className={styles.name}>{`${firstName} ${lastName}`}</h3>
             <p className={styles.years}>
                 {birthYear} - {deathYear}
             </p>
