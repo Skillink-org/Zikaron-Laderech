392bb6a9bcd8473db496c232a16af0cf82774f25
shahaf noked <shahafnoked@gmail.com>
2025-01-27 18:41:35 +0200

    Refactor Search component to use SearchInput component

--- app/components/Header/Search.jsx
+++ app/components/Header/Search.jsx
@@ -1,14 +1,16 @@
 'use client'
 import styles from './style.module.scss'
 import Image from "next/image"
+import SearchInput from '../SearchInput'
 
 export default function Search() {
-    function handleSearch() { }
+    function toggleSearch() { }
+    //TODO toggle Search field on mobile
 
     return (
         <div className={styles.search}>
-            <input type="text" className={styles.searchInput} placeholder="חיפוש" name='search' />
-            <Image src="/search-icon.svg" className={styles.searchIcon} width={26} height={26} alt="Search icon" onClick={handleSearch} />
+            <SearchInput />
+            <Image src="/search-icon.svg" className={styles.searchIcon} width={26} height={26} alt="Search icon" onClick={toggleSearch} />
         </div>
     )
 }
