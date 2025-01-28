86455a4bfb71eb5bb7a6bc3848b8046f8c1e4f6a
DESKTOP-5Q9K2NL\user1 <chay6865@gmail.com>
2025-01-28 20:47:00 +0200

    Usong Next-Image, and reduce the size of card.

--- app/components/ProfileCard/index.jsx
+++ app/components/ProfileCard/index.jsx
@@ -1,21 +1,28 @@
 import styles from "./style.module.scss";
+import Image from "next/image";
 
 export default function ProfileCard({
-  firstName = "מאי",
-  lastName = "הלל",
-  birthYear = "2000",
-  deathYear = "2023",
-  imageSrc = "/profileImage.webp",
+    firstName = "מאי",
+    lastName = "הלל",
+    birthYear = "2000",
+    deathYear = "2023",
+    imageSrc = "/profileImage.webp",
 }) {
-  return (
-    <div className={styles.card}>
-      <div className={styles.profileImage}>
-        <img src={imageSrc} alt={`${firstName} ${lastName}`} />
-      </div>
-      <h3 className={styles.name}>{`${firstName} ${lastName}`}</h3>
-      <p className={styles.years}>
-        {birthYear} - {deathYear}
-      </p>
-    </div>
-  );
+    return (
+        <div className={styles.card}>
+            <div className={styles.profileImage}>
+                <Image
+                    className={styles.profileImage}
+                    src={imageSrc}
+                    alt={`${firstName} ${lastName}`}
+                    width={150}
+                    height={200}
+                />
+            </div>
+            <h3 className={styles.name}>{`${firstName} ${lastName}`}</h3>
+            <p className={styles.years}>
+                {birthYear} - {deathYear}
+            </p>
+        </div>
+    );
 }
