6f32abc440d674890e96cb4ae230b1fe139ae1d1
YakovVazan <yakovvazan@gmail.com>
2025-02-04 00:05:26 +0200

    improvement: added more minor visual improvements to the popup

--- app/components/AuthPopup/index.jsx
+++ app/components/AuthPopup/index.jsx
@@ -99,8 +99,8 @@ export default function AuthPopup({ onClose }) {
           >
             <Image
               src="/google-icon.svg"
-              width={20}
-              height={20}
+              width={16}
+              height={16}
               alt="google icon"
               unoptimized
             />
