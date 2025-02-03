8bebd0101c8ff4f231ee317b8632c289460f5da7
DESKTOP-5Q9K2NL\user1 <chay6865@gmail.com>
2025-02-03 19:40:43 +0200

    Add loading, error message and remove phone field from FormFields

--- app/components/AuthPopup/index.jsx
+++ app/components/AuthPopup/index.jsx
@@ -4,11 +4,11 @@ import GenericInput from "../GenericInput/index";
 import Button from "../Button";
 import { loginFields, signupFields } from "@/lib/FormFields";
 import { signIn } from "next-auth/react";
-import GoogleIcon from '@mui/icons-material/Google'
+import GoogleIcon from '@mui/icons-material/Google';
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function AuthPopup({ onClose }) {
     const [isLogin, setIsLogin] = useState(true);
-
     const [formData, setFormData] = useState({
         email: "",
         password: "",
@@ -16,6 +16,9 @@ export default function AuthPopup({ onClose }) {
         lastName: "",
         phone: "",
     });
+
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
     const handleChange = (key) => (e) => {
         setFormData((prev) => ({
@@ -26,14 +29,23 @@ export default function AuthPopup({ onClose }) {
 
     const toggleForm = () => {
         setIsLogin(!isLogin);
-    }
+        setErrorMessage("");
+    };
 
     const handleClick = async (e) => {
         e.preventDefault();
+        setLoading(true);
+        setErrorMessage("");
+
         if (isLogin) {
             await handleEmailSignin();
         }
-    }
+        else {
+            await handleSignUp();
+        }
+        setLoading(false);
+    };
+
     const handleEmailSignin = async () => {
         const response = await signIn("credentials", {
             redirect: false,
@@ -42,10 +54,11 @@ export default function AuthPopup({ onClose }) {
         });
 
         if (response?.error) {
-            console.error("Email login failed:", response.error);
+            setErrorMessage("התחברות נכשלה. בדוק את הפרטים ונסה שוב.");
         } else {
-            console.log("Logged in with Email:", response);
-            onClose();
+            setTimeout(() => {
+                onClose();
+            }, 2000);
         }
     };
 
@@ -69,14 +82,24 @@ export default function AuthPopup({ onClose }) {
                         />
                     ))}
 
-                    <Button type="submit" onClick={handleClick} className={styles.submitButton}>
-                        {isLogin ? "התחברות" : "הרשמה"}
+                    <Button
+                        type="submit"
+                        onClick={handleClick}
+                        className={styles.submitButton}
+                        disabled={loading}>
+                        {loading ? <CircularProgress className={styles.progressCircle} /> : (isLogin ? "התחברות" : "הרשמה")}
                     </Button>
 
-                    <Button onClick={() => { signIn("google") }} className={styles.googleButton}>
-                        <GoogleIcon />
-                        <p className={styles.googleText}>כניסה עם גוגל</p>
+                    <Button
+                        onClick={() => signIn("google")}
+                        className={styles.googleButton}>
+                        <div className={styles.wrapper}>
+                            <GoogleIcon />
+                            <p className={styles.googleText}>כניסה עם גוגל</p>
+                        </div>
                     </Button>
+
+                    {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
 
                     <div className={styles.toggleButton} onClick={toggleForm}>
                         <small>{isLogin ? "אין לך חשבון? לחץ כאן להרשמה" : "נרשמת בעבר? לחץ כאן להתחברות"}</small>
@@ -86,4 +109,3 @@ export default function AuthPopup({ onClose }) {
         </div>
     );
 }
-
