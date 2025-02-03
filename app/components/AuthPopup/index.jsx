9dd0f4a7c5ec3e234181876626c381155ef397d4
YakovVazan <yakovvazan@gmail.com>
2025-02-03 23:31:32 +0200

    feat: replaced 3 unnecessary dependencies in svg files (to get social icons)

--- app/components/AuthPopup/index.jsx
+++ app/components/AuthPopup/index.jsx
@@ -1,89 +1,108 @@
+import Image from "next/image";
+import Button from "../Button";
 import React, { useState } from "react";
 import styles from "./style.module.scss";
-import GenericInput from "../GenericInput/index";
-import Button from "../Button";
-import { loginFields, signupFields } from "@/lib/FormFields";
 import { signIn } from "next-auth/react";
-import GoogleIcon from '@mui/icons-material/Google'
+import GenericInput from "../GenericInput/index";
+import { loginFields, signupFields } from "@/lib/FormFields";
 
 export default function AuthPopup({ onClose }) {
-    const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(true);
 
-    const [formData, setFormData] = useState({
-        email: "",
-        password: "",
-        firstName: "",
-        lastName: "",
-        phone: "",
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    phone: "",
+  });
+
+  const handleChange = (key) => (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [key]: e.target.value,
+    }));
+  };
+
+  const toggleForm = () => {
+    setIsLogin(!isLogin);
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (isLogin) {
+      await handleEmailSignin();
+    }
+  };
+  const handleEmailSignin = async () => {
+    const response = await signIn("credentials", {
+      redirect: false,
+      email: formData.email,
+      password: formData.password,
     });
 
-    const handleChange = (key) => (e) => {
-        setFormData((prev) => ({
-            ...prev,
-            [key]: e.target.value,
-        }));
-    };
-
-    const toggleForm = () => {
-        setIsLogin(!isLogin);
+    if (response?.error) {
+      console.error("Email login failed:", response.error);
+    } else {
+      console.log("Logged in with Email:", response);
+      onClose();
     }
+  };
 
-    const handleClick = async (e) => {
-        e.preventDefault();
-        if (isLogin) {
-            await handleEmailSignin();
-        }
-    }
-    const handleEmailSignin = async () => {
-        const response = await signIn("credentials", {
-            redirect: false,
-            email: formData.email,
-            password: formData.password,
-        });
+  const currentFields = isLogin ? loginFields : signupFields;
 
-        if (response?.error) {
-            console.error("Email login failed:", response.error);
-        } else {
-            console.log("Logged in with Email:", response);
-            onClose();
-        }
-    };
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.popup}>
+        <button className={styles.closeButton} onClick={onClose}>
+          ✖
+        </button>
+        <h2 className={styles.title}>{isLogin ? "התחברות" : "הרשמה"}</h2>
+        <form className={styles.form}>
+          {currentFields.map(({ type, placeholder, stateKey }) => (
+            <GenericInput
+              key={stateKey}
+              type={type}
+              placeholder={placeholder}
+              value={formData[stateKey]}
+              onChange={handleChange(stateKey)}
+            />
+          ))}
 
-    const currentFields = isLogin ? loginFields : signupFields;
+          <Button
+            type="submit"
+            onClick={handleClick}
+            className={styles.submitButton}
+          >
+            {isLogin ? "התחברות" : "הרשמה"}
+          </Button>
 
-    return (
-        <div className={styles.overlay}>
-            <div className={styles.popup}>
-                <button className={styles.closeButton} onClick={onClose}>
-                    ✖
-                </button>
-                <h2 className={styles.title}>{isLogin ? "התחברות" : "הרשמה"}</h2>
-                <form className={styles.form}>
-                    {currentFields.map(({ type, placeholder, stateKey }) => (
-                        <GenericInput
-                            key={stateKey}
-                            type={type}
-                            placeholder={placeholder}
-                            value={formData[stateKey]}
-                            onChange={handleChange(stateKey)}
-                        />
-                    ))}
+          <Button
+            onClick={() => {
+              signIn("google");
+            }}
+            className={styles.googleButton}
+          >
+            <Image
+              src="/google-icon.svg"
+              width={20}
+              height={20}
+              alt="google icon"
+              unoptimized
+            />
 
-                    <Button type="submit" onClick={handleClick} className={styles.submitButton}>
-                        {isLogin ? "התחברות" : "הרשמה"}
-                    </Button>
+            <p className={styles.googleText}>כניסה עם גוגל</p>
+          </Button>
 
-                    <Button onClick={() => { signIn("google") }} className={styles.googleButton}>
-                        <GoogleIcon />
-                        <p className={styles.googleText}>כניסה עם גוגל</p>
-                    </Button>
-
-                    <div className={styles.toggleButton} onClick={toggleForm}>
-                        <small>{isLogin ? "אין לך חשבון? לחץ כאן להרשמה" : "נרשמת בעבר? לחץ כאן להתחברות"}</small>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+          <div className={styles.toggleButton} onClick={toggleForm}>
+            <small>
+              {isLogin
+                ? "אין לך חשבון? לחץ כאן להרשמה"
+                : "נרשמת בעבר? לחץ כאן להתחברות"}
+            </small>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
-
