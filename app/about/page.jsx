0e64bbbd633a2b455177d33de8a2adc924535838
=Tsipi <tsw32595@gmail.com>
2025-02-04 23:17:33 +0200

    סיימתי

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,46 +1,61 @@
- "use client"
-import React from 'react';
-import styles from './page.module.scss';  
-import CustomBubble from '../components/CustomBubble';
-import LargeBubble from '../components/LargeBubble';
+"use client";
+import React from "react";
+import styles from "./page.module.scss";
+import { useRouter } from "next/navigation";
+import CustomBubble from "../components/CustomBubble";
+import LargeBubble from "../components/LargeBubble";
+import ImageWithTitle from "../components/ImageWithTitle";
 
 export default function AboutPage() {
+  const router = useRouter();
+
   const handleJoinClick = () => {
-    
+    router.push("/add-fallen");
   };
 
   return (
-    <div className={styles.page}>
-      <div className={styles.imageWithTitle}>
-        <h1 className={styles.title}>אודות המיזם</h1>
-        <p className={styles.subtitle}>תיאור קצר על מטרת המיזם</p>
+    <div>
+      <div>
+        <div className={`${styles.imageWithTitle} `}>
+          <ImageWithTitle
+            imageUrl="https://www.picshare.co.il/s_pictures/img62733.jpg"
+            title="מיזם"
+            subtitle="אודות"
+          />
+        </div>
+
+        <CustomBubble className={styles['element-with-spacing']}>
+          <p>יוור נימרול שפיץ בלאקרין לורפט דומק טולפין. גרוי לופצנין נימרומפ בייסטר יאפ טולפריק סטרומרול. דופלינק בלאורפן וצרופינק קלוי תראפ. גלטור רומפניק שפלאס גראפינג קויילנפ טולפס.</p>
+          <p>לאופארק טולפריק לקה פסטורין בניפר פלורטי לארסן תוטפולינק קלורין. דופרמלייט גובצור מימלפ פשטורפין גלפול ברמנופטר ניבקל. גרוטפינק דלפרין בורסטורפ תולפארק ביופצטרל.</p>
+          <p>לורפט דוםקל פסטורי לורפט יערטל שבניפ. גלורריק פסטנורף עלול מארם רובנה קלוי גינורפ נמלז. גרומפניק טופלינק להפטור פילין קוברט נשדלט גריפון.</p>
+          <p>לארפט מינטול כארפר שניפר טימלקה פינדרולטר תרמלשט. קפראינר לתולינפ לדובפרינק בלורולפריט לדולפום גהפלטוק.</p>
+          <p>לאורפט כלפייני גרביט נימרולס קטור טיימלופן שפולש. גראפונרקט לביברף שטוולפ קפינטל גרינום להופס.</p>
+          <p>סלאפנט סולפינק בשאנן פרנוטפ טימפלור גרופניק. לאורפש פניפר שקופה לאורפלין גראזניק מינלואפר קלאברניפ.</p>
+          <p>גרופנרלין דמופלאש שפיולר עלבריין קרוש סופרנס. פלוספינק אופאל נוקירנט דומפנול רייטורופ טופלר.</p>
+        </CustomBubble>
+
+        <div className={`${styles.bubblesContainer} ${styles['element-with-spacing']}`}>
+          <CustomBubble className={styles['element-with-spacing']}>
+            <h3>כותרת בועה 1</h3>
+            <p>תוכן בועה 1</p>
+          </CustomBubble>
+          <CustomBubble className={styles['element-with-spacing']}>
+            <h3>כותרת בועה 2</h3>
+            <p>תוכן בועה 2</p>
+          </CustomBubble>
+          <CustomBubble className={styles['element-with-spacing']}>
+            <h3>כותרת בועה 3</h3>
+            <p>תוכן בועה 3</p>
+          </CustomBubble>
+        </div>
+
+        <LargeBubble
+          title="הצטרפו להנצחה"
+          subtitle="הצטרפו אלינו והיו חלק מההצלחה"
+          buttonText="הצטרף עכשיו"
+          onButtonClick={handleJoinClick}
+        />
       </div>
-
-      <CustomBubble style={{ marginBottom: '20px' }}>
-        <p>סיפור המיזם: כאן נכנס תיאור המיזם בצורה מסודרת.</p>
-      </CustomBubble>
-
-      <div className={styles.bubbles}>
-        <CustomBubble>
-          <h3>כותרת בועה 1</h3>
-          <p>תוכן בועה 1</p>
-        </CustomBubble>
-        <CustomBubble>
-          <h3>כותרת בועה 2</h3>
-          <p>תוכן בועה 2</p>
-        </CustomBubble>
-        <CustomBubble>
-          <h3>כותרת בועה 3</h3>
-          <p>תוכן בועה 3</p>
-        </CustomBubble>
-      </div>
-
-      <LargeBubble
-        title="הצטרפו להנצחה"
-        subtitle="הצטרפו אלינו והיו חלק מההצלחה"
-        buttonText="הצטרף עכשיו"
-        onButtonClick={handleJoinClick}
-      />
     </div>
   );
 }
