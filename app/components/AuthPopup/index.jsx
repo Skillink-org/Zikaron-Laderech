7d34f41f3f398af82108ba1acfe07f205be3481b
DESKTOP-5Q9K2NL\user1 <chay6865@gmail.com>
2025-02-04 12:16:55 +0200

    fix the function handleEmailSigin

--- app/components/AuthPopup/index.jsx
+++ app/components/AuthPopup/index.jsx
@@ -55,77 +55,77 @@ export default function AuthPopup({ onClose }) {
         onClose();
       }, 2000);
     };
-
-    // Close popup on escape key press
-    useEffect(() => {
-      const handleKeyDown = (event) => {
-        if (event.key === "Escape") {
-          onClose();
-        }
-      };
-
-      document.addEventListener("keydown", handleKeyDown);
-      return () => {
-        document.removeEventListener("keydown", handleKeyDown);
-      };
-    }, [onClose]);
-
-    const currentFields = isLogin ? loginFields : signupFields;
-
-    return (
-      <div className={styles.overlay}>
-        <div className={styles.popup}>
-          <button className={styles.closeButton} onClick={onClose}>
-            ✖
-          </button>
-          <h2 className={styles.title}>{isLogin ? "התחברות" : "הרשמה"}</h2>
-          <form className={styles.form} onSubmit={handleSubmit}>
-            {currentFields.map(({ type, placeholder, stateKey }) => (
-              <GenericInput
-                key={stateKey}
-                type={type}
-                name={type}
-                placeholder={placeholder}
-                value={formData[stateKey]}
-                required={true}
-                onChange={handleChange(stateKey)}
-              />
-            ))}
-            <Button
-              type="submit"
-              onClick={handleClick}
-              className={styles.submitButton}
-              disabled={loading}>
-              {loading ? <CircularProgress className={styles.progressCircle} /> : (isLogin ? "התחברות" : "הרשמה")}
-            </Button>
-
-            <Button
-              onClick={() => {
-                signIn("google");
-              }}
-              className={styles.googleButton}
-            >
-              <Image
-                src="/google-icon.svg"
-                width={16}
-                height={16}
-                alt="google icon"
-                unoptimized
-              />
-
-              <p className={styles.googleText}>כניסה עם גוגל</p>
-            </Button>
-            {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
-            <div className={styles.toggleButton} onClick={toggleForm}>
-              <small>
-                {isLogin
-                  ? "אין לך חשבון? לחץ כאן להרשמה"
-                  : "נרשמת בעבר? לחץ כאן להתחברות"}
-              </small>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
   }
+
+  // Close popup on escape key press
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [onClose]);
+
+  const currentFields = isLogin ? loginFields : signupFields;
+
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.popup}>
+        <button className={styles.closeButton} onClick={onClose}>
+          ✖
+        </button>
+        <h2 className={styles.title}>{isLogin ? "התחברות" : "הרשמה"}</h2>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          {currentFields.map(({ type, placeholder, stateKey }) => (
+            <GenericInput
+              key={stateKey}
+              type={type}
+              name={type}
+              placeholder={placeholder}
+              value={formData[stateKey]}
+              required={true}
+              onChange={handleChange(stateKey)}
+            />
+          ))}
+          <Button
+            type="submit"
+            onClick={handleClick}
+            className={styles.submitButton}
+            disabled={loading}>
+            {loading ? <CircularProgress className={styles.progressCircle} /> : (isLogin ? "התחברות" : "הרשמה")}
+          </Button>
+
+          <Button
+            onClick={() => {
+              signIn("google");
+            }}
+            className={styles.googleButton}
+          >
+            <Image
+              src="/google-icon.svg"
+              width={16}
+              height={16}
+              alt="google icon"
+              unoptimized
+            />
+
+            <p className={styles.googleText}>כניסה עם גוגל</p>
+          </Button>
+          {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
+          <div className={styles.toggleButton} onClick={toggleForm}>
+            <small>
+              {isLogin
+                ? "אין לך חשבון? לחץ כאן להרשמה"
+                : "נרשמת בעבר? לחץ כאן להתחברות"}
+            </small>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
