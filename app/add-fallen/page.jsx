7bf919a58e8a5ec54102b7e1312d710b53388b80
Your Name <you@example.com>
2025-02-06 16:42:55 +0200

    zod fallenSchema

--- app/add-fallen/page.jsx
+++ app/add-fallen/page.jsx
@@ -6,13 +6,8 @@ import Button from "../components/Button";
 import StatusMessage from "../components/StatusMessage";
 import CustomBubble from "../components/CustomBubble";
 import { uploadImage } from "@/server/actions/uploadImage.action";
-import { z } from "zod";
 import { addFallen } from "@/server/actions/addFallen.action";
-
-
-
-// TODO-YOSEF: replace fetch with action
-// TODO-YOSEF: replace form manual validation with zod
+import { fallenSchema } from "@/lib/zod/fallenSchema";
 
 
 export default function AddFallenPage() {
@@ -32,43 +27,11 @@ export default function AddFallenPage() {
     imageFile: null
   });
 
-  const fallenSchema = z.object({
-    fullName: z.string().min(5, "נא להזין שם מלא (שם פרטי ושם משפחה)").regex(/^[a-zA-Zא-ת\s]*$/, "שם יכול להכיל רק אותיות ורווחים"),
-    birthYear: z.string().min(4, "נא להזין שנת לידה תקינה").refine(val => {
-      const year = parseInt(val, 10);
-      return year > 0 && year <= new Date().getFullYear();
-    }, "שנת הלידה לא תקינה"),
-    deathDate: z.string().min(1, "נא להזין תאריך פטירה"),
-    hobbies: z.string().min(1, "נא להזין לפחות תחביב אחד").refine(val => val.split(",").length <= 6, "ניתן להזין עד 6 תחביבים"),
-    about: z.string().min(10, "נא להזין מידע על הנופל"),
-    familyMessage: z.string().min(5, "נא להזין מסר מהמשפחה"),
-    imageFile: z.instanceof(File, "נא להעלות תמונה").optional(),
-    highlightQuote: z.string().min(5, "נא להזין ציטוט או משפט חשוב מהנופל"),
-  });
-
   const [statusMessage, setStatusMessage] = useState("");
   const [statusType, setStatusType] = useState(""); // 'success' or 'error'
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-
-    // if (name === "fullName") {
-    //   if (!/^[a-zA-Zא-ת\s]*$/.test(value)) return;
-    // }
-
-    // if (name === "birthYear") {
-    //   const numValue = parseInt(value, 10);
-    //   if (numValue < 0 || numValue > currentYear) return;
-    // }
-
-    // if (name === "hobbies") {
-    //   const hobbiesArray = value
-    //     .split(",")
-    //     .map((hobby) => hobby.trim())
-    //     .filter((hobby) => hobby !== "");
-    //   if (hobbiesArray.length > 6) return;
-    // }
-
     setFormData({ ...formData, [name]: value });
   };
 
@@ -125,22 +88,6 @@ export default function AddFallenPage() {
           name,
           continueCount: 0
         }));
-  
-      // const response = await fetch('/api/add-fallen', {
-      //   method: 'POST',
-      //   headers: { 'Content-Type': 'application/json' },
-      //   body: JSON.stringify({
-      //     firstName,
-      //     lastName,
-      //     birthDate: new Date(formData.birthYear),
-      //     deathDate: new Date(formData.deathDate),
-      //     hobbies,
-      //     about: formData.about,
-      //     familyWords: formData.familyMessage,
-      //     imageUrl,
-      //     isAccepted: false
-      //   }),
-      // });
 
       const response = await addFallen({
         fullName: formData.fullName,
@@ -181,130 +128,6 @@ export default function AddFallenPage() {
     }
   };
   
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   if (!validateForm()) return;
-
-  //   try {
-  //     setStatusMessage("מעלה תמונה...");
-  //     setStatusType("loading");
-
-  //     const formDataToSend = new FormData();
-  //     formDataToSend.append("image", formData.imageFile);
-  //     const imageUrl = await uploadImage(formDataToSend);
-
-  //     //split the fullname into first name and last name
-  //     const [firstName, ...lastNameParts] = formData.fullName.trim().split(" ");
-  //     const lastName = lastNameParts.join(" ");
-
-  //     //format the hobbies into an array of objects
-  //     const hobbies = formData.hobbies
-  //       .split(',')
-  //       .map(hobby => hobby.trim())
-  //       .filter(hobby => hobby)
-  //       .map(name => ({
-  //         name,
-  //         continueCount: 0
-  //       }));
-
-  //     //send the form data to the server
-  //     const response = await fetch('/api/add-fallen', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({
-  //         firstName,
-  //         lastName,
-  //         birthDate: new Date(formData.birthYear),
-  //         deathDate: new Date(formData.deathDate),
-  //         hobbies,
-  //         about: formData.about,
-  //         familyWords: formData.familyMessage,
-  //         imageUrl,
-  //         isAccepted: false
-  //       }),
-  //     });
-
-  //     const result = await response.json();
-
-  //     if (!response.ok) {
-  //       throw new Error(result.error || 'שגיאה בשמירת הנתונים');
-  //     }
-
-  //     setStatusMessage(`הנתונים נשמרו בהצלחה! 
-  //       ${result.data.firstName} ${result.data.lastName} נוסף למאגר וממתין לאישור.`);
-  //     setStatusType("success");
-
-  //     //reset the form
-  //     setTimeout(() => {
-  //       setFormData({
-  //         fullName: "",
-  //         birthYear: "",
-  //         deathDate: "",
-  //         hobbies: "",
-  //         about: "",
-  //         familyMessage: "",
-  //         image: null,
-  //         imageFile: null
-  //       });
-  //     }, 3000);
-
-  //   } catch (error) {
-  //     console.error("שגיאה:", error);
-  //     setStatusMessage(error.message || "אירעה שגיאה. נא לנסות שוב.");
-  //     setStatusType("error");
-  //   }
-  // };
-
-  //validation function
-  // const validateForm = () => {
-  //   const nameParts = formData.fullName.trim().split(" ");
-  //   if (nameParts.length < 2) {
-  //     setStatusMessage("נא להזין שם מלא (שם פרטי ושם משפחה)");
-  //     setStatusType("error");
-  //     return false;
-  //   }
-
-  //   if (!formData.birthYear) {
-  //     setStatusMessage("נא להזין שנת לידה");
-  //     setStatusType("error");
-  //     return false;
-  //   }
-
-  //   if (!formData.deathDate) {
-  //     setStatusMessage("נא להזין תאריך פטירה");
-  //     setStatusType("error");
-  //     return false;
-  //   }
-
-  //   if (!formData.hobbies.trim()) {
-  //     setStatusMessage("נא להזין לפחות תחביב אחד");
-  //     setStatusType("error");
-  //     return false;
-  //   }
-
-  //   if (!formData.about.trim()) {
-  //     setStatusMessage("נא להזין מידע אודות הנופל");
-  //     setStatusType("error");
-  //     return false;
-  //   }
-
-  //   if (!formData.familyMessage.trim()) {
-  //     setStatusMessage("נא להזין מסר מהמשפחה");
-  //     setStatusType("error");
-  //     return false;
-  //   }
-
-  //   if (!formData.imageFile) {
-  //     setStatusMessage("נא להעלות תמונה");
-  //     setStatusType("error");
-  //     return false;
-  //   }
-
-  //   return true;
-  // };
 
   return (
     <>
--- server/actions/addFallen.action.js
+++ server/actions/addFallen.action.js
@@ -1,19 +1,4 @@
 import * as service from "@/server/service/fallen.service";
-import { z } from "zod";
-
-const fallenSchema = z.object({
-    fullName: z.string().min(5).regex(/^[a-zA-Zא-ת\s]*$/, "שם יכול להכיל רק אותיות ורווחים"),
-    birthYear: z.string().min(4).refine(val => {
-        const year = parseInt(val, 10);
-        return year > 0 && year <= new Date().getFullYear();
-    }, "שנת הלידה לא תקינה"),
-    deathDate: z.string().min(1),
-    hobbies: z.string().min(1).refine(val => val.split(",").length <= 6, "ניתן להזין עד 6 תחביבים"),
-    about: z.string().min(10),
-    familyMessage: z.string().min(5),
-    imageFile: z.instanceof(File, "נא להעלות תמונה").optional(),
-    highlightQuote: z.string().min(5),
-});
 
 export async function addFallen(data) {
     const parsed = fallenSchema.safeParse(data);
