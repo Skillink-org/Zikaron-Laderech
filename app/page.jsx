a2aa99084eb10f3a240dad6d2db5ee3b094fb861
RefaelBZ <145847817+refaelBZ@users.noreply.github.com>
2025-02-11 15:35:05 +0200

    Revert "Fallen schema"

--- app/page.jsx
+++ app/page.jsx
@@ -3,41 +3,35 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./page.module.scss";
-import FallenPage from "./components/FallenPage";
 import Button from "./components/Button";
 import HobbyList from "./HomePage/HobbyList";
 
 const HomePage = () => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <FallenPage />
-        <div className={styles["header-image"]}>
-          <Image
-            src="/Home-Page.png"
-            alt="דף הבית"
-            width={1400}
-            height={100}
-            className={styles["Home-image"]}
-          />
-        </div>
+    <div className={styles["Home-container"]}>
+      <div className={styles["header-image"]}>
+        <Image
+          src="/Home-Page.png"
+          alt="דף הבית"
+          width={1400}
+          height={100}
+          className={styles["Home-image"]}
+        />
+      </div>
+      <div className={styles["search-border"]}></div>
+      <HobbyList />
 
-        <div className={styles["search-border"]}></div>
-        <HobbyList />
-
-        {/* קטע ההצטרפות לעמוד הנופלים */}
-        <div className={styles.memorySection}>
-          <div className={styles.memoryTitle}>הצטרפו להנצחה</div>
-          <div className={styles.memoryText}>
-            יחד נמשיך את המורשת של יקירינו דרך התחביבים והסיפורים שהשאירו אחריהם
-          </div>
-          <Button className={styles.memoryButton}>
-            <Link href="/all-fallen">לעמוד הנופלים</Link>
-          </Button>
+      {/* קטע ההצטרפות לעמוד הנופלים */}
+      <div className={styles.memorySection}>
+        <div className={styles.memoryTitle}>הצטרפו להנצחה</div>
+        <div className={styles.memoryText}>
+          יחד נמשיך את המורשת של יקירינו דרך התחביבים והסיפורים שהשאירו אחריהם
         </div>
-      </main>
+        <Button className={styles.memoryButton}>
+          <Link href="/all-fallen">לעמוד הנופלים</Link>
+        </Button>
+      </div>
     </div>
   );
 };
-
 export default HomePage;
