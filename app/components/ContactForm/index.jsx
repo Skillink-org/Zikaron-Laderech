9b83d5fa339287db2ff2d89195f2a6431e85e4f2
MICHALI <michali13.t@gmail.com>
2025-02-11 19:58:57 +0200

    server-update

--- app/components/ContactForm/index.jsx
+++ app/components/ContactForm/index.jsx
@@ -1,15 +1,16 @@
 import styles from './style.module.scss';
+import { submitContactForm } from "@/server/serverContact";
 
 const ContactForm = () => {
   return (
-    <form className={styles["contact-form"]}>
+    <form action={submitContactForm} className={styles["contact-form"]}>
         <div className={styles["form-row"]}>
-      <input type="text" placeholder="שם מלא" required />
-      <input type="email" placeholder="אימייל" required />
-      <input type="tel" placeholder="טלפון" required />
+        <input type="text" name="fullName" placeholder="שם מלא" required />
+        <input type="email" name="email" placeholder="אימייל" required />
+        <input type="tel" name="phone" placeholder="טלפון" required />
       </div>
-      <input type="text" placeholder="נושא" required />
-      <textarea placeholder="תוכן הפניה"></textarea>
+      <input type="text" name="subject" placeholder="נושא" required />
+      <textarea name="message" placeholder="תוכן הפניה"></textarea>
       <button type="submit">שליחה</button>
     </form>
   );
--- server/connect.js
+++ server/connect.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export const connectToMongo = async () => {
   try {
@@ -9,4 +9,4 @@ export const connectToMongo = async () => {
   } catch (error) {
     console.log("Connection error:", error);
   }
-}
+} 
