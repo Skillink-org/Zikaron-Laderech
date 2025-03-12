5d7d62f0c3dad6212280adc0d6e2d0bd94d6d070
etiyoungreis <etiyoungreis@gmail.com>
2025-03-12 13:36:35 +0200

    Refactor FallenPage to use service and remove unnecessary checks.

--- app/all-fallen/[fallen]/page.jsx
+++ app/all-fallen/[fallen]/page.jsx
@@ -1,4 +1,4 @@
-import { getFallenById } from "@/server/actions/fallen.action";
+import { getFallenById } from "@/server/service/fallen.action";
 import { connectToDB } from "@/server/connect";
 import { FallenProvider } from '@/app/contexts/FallenContext';
 import HobbyDataList from "@/app/components/HobbyDataList";
@@ -14,47 +14,44 @@ export default async function FallenPage({ params }) {
   // TODO - bonus - use name of fallen instead of id
   const fallenId = (await params).fallen;
   // TODO - use service not action
-  const response = await getFallenById(fallenId);
+  const fallen = await getFallenById(fallenId);
   
   // TODO - no need to use response.ok 
-  if (!response.ok)
-    return <StatusMessage message={response.message} type="error" />;
-  
-  // TODO - no need to use response.ok 
-  const fallenDetails = response.data;
+  if (!fallen)
+    return <StatusMessage message='הנופל לא נמצא' type="error" />;
 
   return (
     // TODO -  why ? - no need of provider - just pass the data by props
-    <FallenProvider initialHobbies={fallenDetails.hobbies}>
+    <FallenProvider initialHobbies={fallen.hobbies}>
       <div className={styles.fallen}>
         {/* right */}
         <div className={`${styles.rightCol} ${styles.col}`}>
-          <ProfileCard fallen={fallenDetails} />
+          <ProfileCard fallen={fallen} />
           <TitleDivider
             title={"התחביבים שלי"}
             containerClassName={styles.hobbiesDivider}
           />
-          <HobbyList hobbies={fallenDetails.hobbies.map((hobby) => hobby.name)} fallenName={fallenDetails.firstName} fallenId={fallenId} />
+          <HobbyList hobbies={fallen.hobbies.map((hobby) => hobby.name)} fallenName={fallen.firstName} fallenId={fallenId} />
         </div>
         {/* middle */}
         <div className={`${styles.middleCol} ${styles.col}`}>
           <div className={styles.middleColText}>
-            <h1 className={styles.mainTitle}>{fallenDetails.quote}</h1>
+            <h1 className={styles.mainTitle}>{fallen.quote}</h1>
             <TitleDivider
               title={"אודות"}
               dividerClassName={styles.sctionsDivider}
             />
-            <p className={styles.paragraph}>{fallenDetails.about}</p>
+            <p className={styles.paragraph}>{fallen.about}</p>
             <TitleDivider
               title={"קצת עליי"}
               dividerClassName={styles.sctionsDivider}
             />
-            <p className={styles.paragraph}>{fallenDetails.familyWords}</p>
+            <p className={styles.paragraph}>{fallen.familyWords}</p>
           </div>
         </div>
         {/* left */}
         <div className={`${styles.leftCol} ${styles.col}`}>
-          <HobbyDataList fallenName={fallenDetails.firstName} />
+          <HobbyDataList fallenName={fallen.firstName} />
           <ShareButton />
         </div>
       </div>
